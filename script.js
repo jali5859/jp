53bb0e35877dbca73b726d4142fffdeb724dcf48
Justin <relishedarc@gmail.com>
2018-12-10 21:41:43 -0400

    table

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
 $(document).ready(function () {
-     
-        $(function (){
-            let $items = $('#items');
-
+    $( '#tester' ).click(function(){
             $.ajax ({
                 type: 'GET',
                 url: 'https://jsonplaceholder.typicode.com/posts',
                 success: function(items) {
                     $.each(items, function(i, item) {
                         // $items.append('<li>userId: '+ item.userId +', id: '+ item.id +', title: '+ item.title +' body: '+ item.body +' </li>');
-                        const data = `userId: ${item.userId} id: ${item.id} title: ${item.title} body: ${item.body}`;
-                        let elem = $('<tr/>').append( data )
-                        $('#items').append(elem);
+                        const data = `<th scope="row">${item.userId}</th> <td>${item.id}</td> <td>${item.title}</td> <td>${item.body}</td>`;
+                        let elem = $('<tr/>').append( data );
+                        $('#table').append( elem );
                     });
                 }
+                
             });
-        });
-
+    
     });
+});
   
  
